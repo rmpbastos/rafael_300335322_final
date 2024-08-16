7c050a58ab211b68f8b73d39ca37c81332f7f041
Rafael Bastos <47896406+rmpbastos@users.noreply.github.com>
2024-08-16 14:05:52 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,16 +9,16 @@ const app = express()
 
 const PORT = process.env.PORT || 5000;
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
-app.use(cors(
-    {
-        origin: ["https://rafael-300335322-frontend.vercel.app/"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+// app.use(cors(
+//     {
+//         origin: ["https://rafael-300335322-frontend.vercel.app/"],
+//         methods: ["POST", "GET"],
+//         credentials: true
+//     }
+// ));
 
 app.use(express.json())
 
